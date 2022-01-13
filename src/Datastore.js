c6aec70a39f7d52a0a6a461b3f5cf891dbe85379
Kshounish Mangal <kshounish36@gmail.com>
2022-01-13 13:52:33 +0530

    [added grid]

--- src/Datastore.js
+++ src/Datastore.js
@@ -20,10 +20,13 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { Paper } from "@mui/material";
 import Image from './website_background.jpg';
 import FALLBACK_IMAGE from "./accdetails.png";
+import FALLBACK_IMAGE1 from "./bill.png";
 import SearchAppBar from './Appbar';
+import Grid from '@mui/material/Grid';
 
 const Datastore = () => {
     const onMediaFallback = event => event.target.src = FALLBACK_IMAGE;
+    const onBillMediaFallback = event => event.target.src = FALLBACK_IMAGE1;
 
     function avatar(str) {
         return str.subString(0,1);
@@ -59,40 +62,80 @@ const Datastore = () => {
         return (
             <div>
                 <SearchAppBar />
-                    <Card sx={{ maxWidth: 400,  ml: 5, mt: 5, backgroundColor: '#56A5EC' }} variant="outlined">
-                        <CardHeader
-                            avatar={
-                            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                            </Avatar>
-                            }
-                            action={
-                            <IconButton aria-label="settings">
-                                <MoreVertIcon />
-                            </IconButton>
-                            }
-                            title={customer[0].firstname}
-                            subheader={customer[0].surname}
-                        />
-                        <CardMedia
-                            component="img"
-                            height="194"
-                            image="./Customer360.png"
-                            alt="E.ON Customer 360"
-                            onError={onMediaFallback}
-                        />
-                        <CardContent>
-                        <div>
-                            <Typography>First Name: {customer[0].firstname}</Typography>
-                            <Typography>Last Name: {customer[0].surname}</Typography>
-                            <Typography>Street: {customer[0].street}</Typography>
-                            <Typography>House No: {customer[0].houseNumber}</Typography>
-                            <Typography>Phone: {customer[0].phoneMobile}</Typography>
-                            <Typography>Meter Location: {customer[0].street}</Typography>
-                            <Typography>Market Location: {customer[0].city}</Typography>
-                            <Typography>Balance: {customer[0].accountBalance}</Typography>
-                        </div>
-                        </CardContent>
-                    </Card>
+                <Grid container spacing={2}>
+                    <Grid item xs={6} md={4}>
+                        <Card sx={{ maxWidth: 400,  ml: 5, mt: 5, backgroundColor: '#56A5EC' }} variant="outlined">
+                            <CardHeader
+                                avatar={
+                                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                                </Avatar>
+                                }
+                                action={
+                                <IconButton aria-label="settings">
+                                    <MoreVertIcon />
+                                </IconButton>
+                                }
+                                title={customer[0].firstname}
+                                subheader={customer[0].surname}
+                            />
+                            <CardMedia
+                                component="img"
+                                height="194"
+                                image="./Customer360.png"
+                                alt="E.ON Customer 360"
+                                onError={onMediaFallback}
+                            />
+                            <CardContent>
+                            <div>
+                                <Typography>First Name: {customer[0].firstname}</Typography>
+                                <Typography>Last Name: {customer[0].surname}</Typography>
+                                <Typography>Street: {customer[0].street}</Typography>
+                                <Typography>House No: {customer[0].houseNumber}</Typography>
+                                <Typography>Phone: {customer[0].phoneMobile}</Typography>
+                                <Typography>Meter Location: {customer[0].street}</Typography>
+                                <Typography>Market Location: {customer[0].city}</Typography>
+                                <Typography>Balance: {customer[0].accountBalance}</Typography>
+                            </div>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={6} md={4}>
+                        <Card sx={{ maxWidth: 400,  ml: 5, mt: 5, backgroundColor: '#56A5EC' }} variant="outlined">
+                            <CardHeader
+                                avatar={
+                                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                                </Avatar>
+                                }
+                                action={
+                                <IconButton aria-label="settings">
+                                    <MoreVertIcon />
+                                </IconButton>
+                                }
+                                // title={customer[0].firstname}
+                                // subheader={customer[0].surname}
+                            />
+                            <CardMedia
+                                component="img"
+                                height="194"
+                                image="./Customer360.png"
+                                alt="E.ON Customer 360"
+                                onError={onBillMediaFallback}
+                            />
+                            <CardContent>
+                            <div>
+                                {/* <Typography>First Name: {customer[0].firstname}</Typography>
+                                <Typography>Last Name: {customer[0].surname}</Typography>
+                                <Typography>Street: {customer[0].street}</Typography>
+                                <Typography>House No: {customer[0].houseNumber}</Typography>
+                                <Typography>Phone: {customer[0].phoneMobile}</Typography>
+                                <Typography>Meter Location: {customer[0].street}</Typography>
+                                <Typography>Market Location: {customer[0].city}</Typography>
+                                <Typography>Balance: {customer[0].accountBalance}</Typography> */}
+                            </div>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                </Grid>
             </div>
         )
     }
